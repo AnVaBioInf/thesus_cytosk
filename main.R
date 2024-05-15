57087c4bf17f001e2b3813308f41959e4cd77a65
AnVaBioInf <17991837asp@gmail.com>
2024-05-15 07:13:58 +0300

    covarage

--- main.R
+++ main.R
@@ -119,59 +119,29 @@ common_sign_jxns =
   common_sign_jxns[order(common_sign_jxns$gene_name, 
                          common_sign_jxns$junction_id,
                          common_sign_jxns$tissue), ]
-# for every gene
-for (jxn in unique(common_sign_jxns$junction_id)){
-  # setting number of plot rows to number of tissues where selected gene junctions were significant, but no more than 3
-  # selecting significant junctions for the GENE in both, development and cancer
-  sign.jxn.df = common_sign_jxns[common_sign_jxns$junction_id==jxn,]
-  gene = unique(sign.jxn.df$gene_name)
-  par(mfrow = c(min(length(unique(sign.jxn.df$tissue)),3),2), bty='n')
-  # for every tissue where any of selected junctions are significant
-  for (tissue in (unique(sign.jxn.df$tissue))){ 
-    covs.summed.gene = prepareCovs(gene,rse.jxn.cytosk,tissue)
-    
-    fetus.covs.summed.gene =  covs.summed.gene[['fetus.covs.summed.gene']]
-    adult.covs.summed.gene = covs.summed.gene[['adult.covs.summed.gene']]
-    
-    gene.region.coords = strsplit(jxn,'[:-]')
-    gene.region.coords = as.integer(c(gene.region.coords[[1]][2], gene.region.coords[[1]][3]))
-    gene.region.coords = c(gene.region.coords[1], gene.region.coords[2])
-    
-    fetus.covs.summed.gene = set.colors(jxn, fetus.covs.summed.gene)
-    adult.covs.summed.gene = set.colors(jxn, adult.covs.summed.gene)
-    
-    sign.jxn.tissue = sign.jxn.df[sign.jxn.df$tissue==tissue,]
-    
-    text = paste(paste(tissue,gene, jxn), " \n(",
-      'dPSI.sajr=',round(sign.jxn.tissue$dPSI_sajr, digits=2), 
-      'FDR.sajr=', round(sign.jxn.tissue$FDR_sajr, digits=2),
-      'logFC.dje=', round(sign.jxn.tissue$logFC_dje, digits=2),
-      'FDR.dje=', round(sign.jxn.tissue$FDR_dje, digits=2),
-      'sign.diego=', round(sign.jxn.tissue$abund_change_diego, digits=2),
-      'FDR.diego=', round(sign.jxn.tissue$FDR_diego, digits=2), "\n",
-      'dPSI_gtex2tum',round(sign.jxn.tissue$dPSI_gtex2tum, digits=2),
-      'FDR_gtex2tum',round(sign.jxn.tissue$FDR_gtex2tum, digits=2),
-      'dPSI_norm2tum',round(sign.jxn.tissue$dPSI_norm2tum, digits=2),
-      'FDR_norm2tum',round(sign.jxn.tissue$FDR_norm2tum, digits=2), ")")
-    
-    plotReadCov(fetus.covs.summed.gene,
-                junc.col = fetus.covs.summed.gene$cols,
-                xlim=gene.region.coords,
-                plot.junc.only.within = F,
-                min.junc.cov.f = 0.05,
-                sub='Before birth'
-    )
-    mtext(text, side=3, line=1.5, cex=0.7, adj=0) 
-    
-    plotReadCov(adult.covs.summed.gene,
-                junc.col = fetus.covs.summed.gene$cols,
-                xlim=gene.region.coords,
-                plot.junc.only.within = F,
-                min.junc.cov.f = 0.05,
-                sub='After birth')
 
-  }
-}
+# write.table(common_sign_jxns, file = "jxns_common_dev_cancer.csv", 
+#             sep = ",",  row.names = TRUE, col.names = TRUE)
+
+
+
+
+
+dev.off()
+common_sign_jxns_outputs_gtex2tum = findCommonJxns(outputs_gtex2tum)
+common_sign_jxns_outputs_norm2tum = findCommonJxns(outputs_norm2tum)
+
+common_sign_jxns = merge(common_sign_jxns_outputs_gtex2tum,common_sign_jxns_outputs_norm2tum,
+                         by = c('junction_id_sajr', 'tissue', 'junction_id', 'gene_name', 'gene_id',
+                                'dPSI_sajr', 'FDR_sajr', 'logFC_dje', 'FDR_dje',
+                                'abund_change_diego', 'FDR_diego'), all=TRUE)
+common_sign_jxns = 
+  common_sign_jxns[order(common_sign_jxns$gene_name, 
+                         common_sign_jxns$junction_id,
+                         common_sign_jxns$tissue), ]
+
+# write.table(common_sign_jxns, file = "jxns_common_dev_cancer.csv", 
+#             sep = ",",  row.names = TRUE, col.names = TRUE)
 
 
 
--- plotToolsResults.R
+++ plotToolsResults.R
@@ -327,7 +327,7 @@ plotFisherResults = function(fisher_results_tissues_list, thresholds){
 set.colors = function(jxn, covs){
   # colors
   cols = ifelse(sub(':.$', '', rownames(covs$juncs)) %in% jxn,
-                'orange', 'skyblue')
+                'orange2', '#377EB8')
   covs$juncs$cols = cols
   covs
 }
@@ -399,5 +399,60 @@ plotArc = function(from,to,top,n=100,y.base=0,...){
   y = x*4*top/len - x^2*(4*top/len^2) 
   # This equation represents a downward-facing parabola that starts at 0, reaches its peak at top, and ends at 0 again.
   lines(x+from,y+y.base,...)
+}
+
+# for every gene
+runJunxtionPlot = function(common_sign_jxns){
+  for (jxn in unique(common_sign_jxns$junction_id)){
+  # setting number of plot rows to number of tissues where selected gene junctions were significant, but no more than 3
+  # selecting significant junctions for the GENE in both, development and cancer
+  sign.jxn.df = common_sign_jxns[common_sign_jxns$junction_id==jxn,]
+  gene = unique(sign.jxn.df$gene_name)
+  par(mfrow = c(min(length(unique(sign.jxn.df$tissue)),4),2), bty='n')
+  # for every tissue where any of selected junctions are significant
+  for (tissue in (unique(sign.jxn.df$tissue))){ 
+    covs.summed.gene = prepareCovs(gene,rse.jxn.cytosk,tissue)
+    
+    fetus.covs.summed.gene =  covs.summed.gene[['fetus.covs.summed.gene']]
+    adult.covs.summed.gene = covs.summed.gene[['adult.covs.summed.gene']]
+    
+    gene.region.coords = strsplit(jxn,'[:-]')
+    gene.region.coords = as.integer(c(gene.region.coords[[1]][2], gene.region.coords[[1]][3]))
+    gene.region.coords = c(gene.region.coords[1], gene.region.coords[2])
+    
+    fetus.covs.summed.gene = set.colors(jxn, fetus.covs.summed.gene)
+    adult.covs.summed.gene = set.colors(jxn, adult.covs.summed.gene)
+    
+    sign.jxn.tissue = sign.jxn.df[sign.jxn.df$tissue==tissue,]
+    
+    text = paste(paste(tissue,gene, jxn), " \n(",
+                 'dPSI.sajr=',round(sign.jxn.tissue$dPSI_sajr, digits=2), 
+                 ', FDR.sajr=', round(sign.jxn.tissue$FDR_sajr, digits=2),
+                 ', logFC.dje=', round(sign.jxn.tissue$logFC_dje, digits=2),
+                 ', FDR.dje=', round(sign.jxn.tissue$FDR_dje, digits=2),
+                 ', sign.diego=', round(sign.jxn.tissue$abund_change_diego, digits=2),
+                 ', FDR.diego=', round(sign.jxn.tissue$FDR_diego, digits=2), "\n",
+                 ' dPSI_gtex2tum =',round(sign.jxn.tissue$dPSI_gtex2tum, digits=2),
+                 ', FDR_gtex2tum =',round(sign.jxn.tissue$FDR_gtex2tum, digits=2),
+                 ', dPSI_norm2tum =',round(sign.jxn.tissue$dPSI_norm2tum, digits=2),
+                 ', FDR_norm2tum =',round(sign.jxn.tissue$FDR_norm2tum, digits=2), ")")
+    
+    plotReadCov(fetus.covs.summed.gene,
+                junc.col = fetus.covs.summed.gene$cols,
+                xlim=gene.region.coords,
+                plot.junc.only.within = F,
+                min.junc.cov.f = 0.05,
+                sub='Before birth'
+    )
+    mtext(text, side=3, line=0.5, cex=0.7, adj=0) 
+    
+    plotReadCov(adult.covs.summed.gene,
+                junc.col = fetus.covs.summed.gene$cols,
+                xlim=gene.region.coords,
+                plot.junc.only.within = F,
+                min.junc.cov.f = 0.05,
+                sub='After birth')
+    }
+  }
 }
 
